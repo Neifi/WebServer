b1790118ad57c924d6ac73047d3e39e07805c292
Neifi <44041549+Neifi@users.noreply.github.com>
2020-04-05 00:47:40 +0200

    Add files via upload
    send ip

--- serverMail.py
+++ serverMail.py
@@ -17,27 +17,27 @@ except ImportError:
     # Handle Python 2.x
     import SimpleHTTPServer as server
 
-class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
-    
-    def controller(self):
 
-        
+class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
+
+    def sendEmail(self):
 
         # login
-        print("Usuario")
-        user = input()
-        print("Password")
-        password = 'dyzoniannxmgbxus'
+        user = 'mi_gmail'
+        password = 'mi_appkey_de_google'
 
         # create mail
         print("[INFO] Generando email")
         msg = MIMEMultipart()
 
-        msg['subject'] = 'Consulta'
+        #get ip
+        ip = str(self.client_address).split("'")[1]
+
+        msg['subject'] = "Nueva conexión"
         msg['From'] = user
         msg['To'] = 'reytax96@gmail.com'
-        
-        
+        msg.attach(MIMEText("Nueva conexión desde la dirección ip: "+ip, 'plain'))
+
         # send mail
         print("[INFO] Enviando email")
         sender = smtplib.SMTP(host='smtp.gmail.com', port=587)
@@ -46,23 +46,19 @@ class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
         sender.send_message(msg)
         del msg
         sender.quit()
-        
-        self.send_response(200)
-    
-    def do_GET(self):
-        self.post()
 
-    def post(self):
-        
-        self.controller()
+        self.send_response(200)
+
+    def do_GET(self):
+        self.load()
+        self.sendEmail()
+
+    def load(self):
         filename = os.path.basename("webServer/file.txt")
-        
         open(filename, 'wb')
-        
         self.send_response(201, 'Created')
         self.end_headers()
-        reply_body = 'Saved "%s"\n' % filename
-        self.wfile.write(reply_body.encode('utf-8'))
+
 
 def run(server_class=HTTPServer, handler_class=HTTPRequestHandler, addr="localhost", port=8000):
     print("Running server in " + addr + "port " + str(port))
